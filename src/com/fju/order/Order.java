375be302e627d41b2c665400073baf3d8ec97ac6
silent22134 <firenba46@gmail.com>
2022-04-29 16:04:30 +0800

    Order Runner

--- src/com/fju/order/Order.java
+++ src/com/fju/order/Order.java
@@ -3,9 +3,15 @@ package com.fju.order;
 public class Order {
     int amount;
     Delivery delivery;
+    int shipFeed;
+
+    public int total(){
+        return amount+shipFeed;
+    }
 
     public Order(int amount, Delivery delivery) {
         this.amount = amount;
         this.delivery = delivery;
+        this.shipFeed = delivery.price;
     }
 }
---
+++ src/com/fju/order/ShopeeDelivery.java
@@ -0,0 +1,10 @@
+package com.fju.order;
+
+public class ShopeeDelivery extends Delivery{
+    public ShopeeDelivery(){
+        name = "Shopee";
+        price = 19;
+    }
+    
+
+}
